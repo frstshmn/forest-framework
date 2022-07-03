13e154030fa8351e0e3dc95de5940ff04ea44c8f
frstshmn <korenha.v@gmail.com>
2022-07-04 00:26:55 +0300

    Added DB functionality

--- core/Core.php
+++ core/Core.php
@@ -10,20 +10,21 @@ class Core {
     function __construct( $version ) {
         $this->version = $version;
         $this->config = $this->config();
+        $this->db_connect();
     }
 
     function config(): object {
         return json_decode( file_get_contents( "config.json" ) )->{$this->version};
     }
 
-    function get_version(): string {
-        return $this->version;
-    }
-
     function run_routes() {
         global $CORE_VERSION;
         $CORE_VERSION = $this->version;
         include ( helper()->api_folder() . $this->version . "/routes.php" );
+    }
+
+    function db_connect() {
+        DB()->connect( $this->config->host, $this->config->user, $this->config->password, $this->config->database, $this->config->port);
     }
 
 }
--- core/DB.php
+++ core/DB.php
@@ -1,5 +1,37 @@
 <?php
 
 class DB {
+    private PDO $db;
+    protected static $_instance = null;
 
+    public static function instance() {
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    public function connect( $host, $user, $password, $database, $port = 3306 ) {
+        $this->db = new PDO('mysql:host=' . $host . ';port=' . $port . ';dbname=' . $database, $user, $password);
+    }
+
+    public function query( $query ) {
+        $sql = $this->db->prepare( $query );
+        $result = $sql->execute();
+
+        if ( strpos( strtoupper( $query ), "SELECT" ) !== 0 ) {
+            return $result;
+        } else {
+            return $sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+    }
+}
+
+function DB() {
+    return DB::instance();
+}
+
+function SQL( $query ) {
+    return DB()->query( $query );
 }
